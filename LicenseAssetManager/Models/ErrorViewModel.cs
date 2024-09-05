43d34c893ec6df85716a78a25fe3eaea44c8dc11
John Williams <johnlwilliamsjr2013@gmail.com>
2024-09-04 21:02:16 -0600

    Add project files.

---
+++ LicenseAssetManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LicenseAssetManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
