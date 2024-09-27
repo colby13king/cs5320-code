9b61262a8c8c09b5ff7a17ca36e80270daf85610
John Williams <johnlwilliamsjr2013@gmail.com>
2024-09-27 09:20:10 -0600

    Added bootstrap styling support. Used bootstrap style in _Layout.cshtm added style to the buttons generated by thee PageLinkTagHelper class. Created and used partial view ProductSummary.cshtml

--- LicenseAssetManager/Infrastructure/PageLinkTagHelper.cs
+++ LicenseAssetManager/Infrastructure/PageLinkTagHelper.cs
@@ -27,6 +27,11 @@ namespace LicenseAssetManager.Infrastructure
 
         public string? PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal {  get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -37,6 +42,13 @@ namespace LicenseAssetManager.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+
+                    if(PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
