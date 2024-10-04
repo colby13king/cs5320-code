9cef02bcf46ac92c661531cad178ea1eb8085df2
John L Williams <jwilli11@uccs.edu>
2024-10-03 20:35:08 -0600

    Rider changes

--- TestLicenseAssetManager/UnitTestProductsListViewModel.cs
+++ TestLicenseAssetManager/UnitTestProductsListViewModel.cs
@@ -1,6 +1,21 @@
+using LicenseAssetManager.Models;
+using LicenseAssetManager.ViewModels;
+
 namespace TestLicenseAssetManager;
 
 public class UnitTestProductsListViewModel
 {
-    
+    [Fact]
+    public void TestProductListViewModelCreation()
+    {
+        // Arrange
+        ProductsListViewModel viewModel = new ProductsListViewModel();
+        viewModel.Products = new List<Product>();
+        
+        // Act
+
+        // Assert
+        Assert.NotNull(viewModel);
+        Assert.NotNull(viewModel.Products);
+    }
 }
