515caa3f11c7301035462bd1419a8b98e3bdfb86
John Williams <johnlwilliamsjr2013@gmail.com>
2024-10-07 22:02:37 -0600

    Added unit test for Can_Filter_Products

--- TestLicenseAssetManager/UnitTestHomeController.cs
+++ TestLicenseAssetManager/UnitTestHomeController.cs
@@ -94,5 +94,44 @@ namespace TestLicenseAssetManager
             Assert.Equal(5, pageInfo.TotalItems);
             Assert.Equal(2, pageInfo.TotalPages);
         }
+
+        [Fact]
+
+        /// <remarks>
+        /// Author:      John L Williams Jr
+        /// Date:        10/7/2024
+        /// Method Name: Can_Filter_Products
+        /// Description: This test creates a mock repository containing Product objects that belong to a range of categories.
+        /// One specific category is requested using the action method, and the results are checked to ensure that the results
+        /// are the right objects in the right order. 
+        /// This test the category filtering function to ensure that the filter can correctly generate products in a spceified category.
+        /// Arguments:   none
+        /// </remarks>
+
+        public void Can_Filter_Products()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns(
+                (new Product[]
+                {
+                    new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                    new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                    new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                    new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                    new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+                }).AsQueryable<Product>()
+                );
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act
+            Product[] result = (controller.Index("Cat2", 1)?.ViewData.Model as ProductsListViewModel ?? new()).Products.ToArray();
+            
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
     }
 }
