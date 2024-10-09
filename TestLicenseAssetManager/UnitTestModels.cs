cf54c1e41a05d145b5d8be8ddd1da1b3c7f0553b
John Williams <johnlwilliamsjr2013@gmail.com>
2024-10-08 21:47:38 -0600

    Added Unit Test for Categories and ViewModels Updated Unit Test Models; Add test for Product.

--- TestLicenseAssetManager/UnitTestModels.cs
+++ TestLicenseAssetManager/UnitTestModels.cs
@@ -9,13 +9,28 @@ namespace TestLicenseAssetManager
         {
             ErrorViewModel errorViewModel = new ErrorViewModel();
             Assert.NotNull(errorViewModel);
+        }
 
+        [Fact]
+        public void TestProduct()
+        {
+            // Arrange
+            Product product = new Product()
+            {
+                ProductID = 1,
+                Category = "Cat1",
+                Price = 1,
+                Name = "P1",
+                Description = "Product Description"
+            };
+
+            // Assert
+            Assert.NotNull(product);
+            Assert.Equal(1, product.ProductID);
+            Assert.Equal("Cat1", product.Category);
+            Assert.Equal(1, product.Price);
+            Assert.Equal("P1", product.Name);
+            Assert.Equal("Product Description", product.Description);
         }
     }
-}
-
-// Test Push
-public class Test()
-{
-    
 }
---
+++ TestLicenseAssetManager/UnitTestViewModels.cs
@@ -0,0 +1,100 @@
+﻿using LicenseAssetManager.Controllers;
+using LicenseAssetManager.Models;
+using LicenseAssetManager.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLicenseAssetManager
+{
+    public class UnitTestViewModels
+    {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+
+            mock.Setup(m => m.Products).Returns(
+                (new Product[]
+                {
+                    new() {ProductID = 1, Name = "P1", Category = "Apples"},
+                    new() {ProductID = 2, Name = "P2", Category = "Apples"},
+                    new() {ProductID = 3, Name = "P3", Category = "Plums"},
+                    new() {ProductID = 4, Name = "P4", Category = "Oranges"},
+                    new(){ProductID = 5, Name = "P5", Category = "Oranges"}
+                }).AsQueryable<Product>()
+                );
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductsListViewModel result = controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+
+            mock.Setup(m => m.Products).Returns(
+                (new Product[]
+                {
+                    new() {ProductID = 1, Name = "P1", Category = "Apples"},
+                    new() {ProductID = 2, Name = "P2", Category = "Apples"}
+                }).AsQueryable<Product>()
+                );
+
+            HomeController controller = new HomeController(mock.Object);
+
+            // Act
+            ProductsListViewModel result = controller.Index(null)?.ViewData.Model as ProductsListViewModel ?? new();
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2 );
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+
+            mock.Setup(m => m.Products).Returns(
+                (new Product[]
+                {
+                    new() {ProductID = 1, Name = "P1", Category = "Apples"},
+                    new() {ProductID = 2, Name = "P2", Category = "Apples"},
+                    new() {ProductID = 3, Name = "P3", Category = "Plums"},
+                    new() {ProductID = 4, Name = "P4", Category = "Oranges"},
+                    new() {ProductID = 5, Name = "P5", Category = "Oranges"}
+                }).AsQueryable<Product>()
+                );
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act
+            ProductsListViewModel result = controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
