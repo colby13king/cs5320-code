ebdb43b5cc725e078c3c4b02481fba9642678a14
John L Williams <jwilli11@uccs.edu>
2024-11-04 09:19:46 -0700

    Updated SDK to actually try to connect to the real web service to get license. If connection is made, license is granted. Next step is to confirm that user has an account.

--- LicenseAssetManagerWpfTestApp/MainWindow.xaml.cs
+++ LicenseAssetManagerWpfTestApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using LicenseAssetManagerSDK.Controllers;
 using LicenseAssetManagerSDK.Models;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -19,47 +20,77 @@ namespace LicenseAssetManagerWpfTestApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
-            licenseManager = new LicenseManager();
+            licenseManager = new LicenseAssetManagerSDK.Controllers.LicenseManager();
 
             licenseNamesLB.SelectedIndex = 0;
+
+            emailAddress = "jwilli11@uccs.edu";
+
+            this.DataContext = this;
         }
 
 
         // This is for LicenseManager
-        private LicenseManager licenseManager;
+        private LicenseAssetManagerSDK.Controllers.LicenseManager licenseManager;
 
         /// <remarks>
         /// This is for LicenseManager
         /// </remarks>
 
-        public LicenseManager LicenseManager
+        public LicenseAssetManagerSDK.Controllers.LicenseManager LicenseManager
         {
             get => licenseManager;
             set { licenseManager = value; }
         }
 
-        static readonly string url = "http://www.google.com";
+        static readonly string url = "http://localhost:5228";
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        // 
+        private string emailAddress;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <remarks>
+        /// 
+        /// </remarks>
+
+        public string EmailAddress
         {
+            get => emailAddress;
+            set { 
+                emailAddress = value;
+                OnPropertyChanged("EmailAddress");
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            requestResult.Content = "Processing License Request...";
             string? licenseName = (licenseNamesLB.Items.GetItemAt(licenseNamesLB.SelectedIndex) as ListBoxItem)?.Content.ToString();
             //MessageBox.Show($"Attempting to get license for {licenseName}");
             int PID = 1;
-            requestString.Content = url + ":" + emailTB.Text + ":" + PID + ":" + licenseName;
-            License license = LicenseManager.GetLicense(url, emailTB.Text, 1, licenseName);
+            requestString.Content = url + ":" + emailAddress + ":" + PID + ":" + licenseName;
+            LicenseAssetManagerSDK.Models.License license = await LicenseManager.GetLicense(url, emailAddress, 1, licenseName);
             //MessageBox.Show("License object obtained");
-            if (!license.Approved)
+            if (license.Approved)
+            {
+                requestResult.Content = "License Approved!";
+            }
+            else
             {
                 requestResult.Content = "License Denied!";
             }
-
-            //MessageBox.Show("License approved");
-            requestResult.Content = "License Approved!";
         }
     }
 }
